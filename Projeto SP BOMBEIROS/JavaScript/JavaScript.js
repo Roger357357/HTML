a09c3b9be01f7ea06bd88506d481293f090f9359
Roger357357 <32617779+Roger357357@users.noreply.github.com>
2019-01-02 21:37:33 -0200

    Atualização 02/01

---
+++ Projeto SP BOMBEIROS/JavaScript/JavaScript.js
@@ -0,0 +1,5 @@
+// alert("Olá, mundo!")
+// var nome = prompt('Qual é o seu nome?')
+// var idade = prompt('Qual é a sua idade?')
+
+// document.write('<h1> Olá ' + nome + ', Tudo Bem? Estou vendo aqui que você possuí ' + idade + ' anos.')
